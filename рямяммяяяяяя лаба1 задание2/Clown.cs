699f3584af5f6ed48ff2e0d47f2b7030ee9a6ac5
a_лина <118667639+naflexsil@users.noreply.github.com>
2023-02-11 21:15:10 +0700

    ОБНОВЛЕННАЯ ЛАБА1 ЗАДАНИЕ 2

--- рямяммяяяяяя лаба1 задание2/Clown.cs
+++ рямяммяяяяяя лаба1 задание2/Clown.cs
@@ -1,23 +1,23 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp {
     class Clown {           
         private static void Main(string[] args) {
             Console.Write("введите число: ");
             int x = int.Parse(Console.ReadLine());  // из строки вычисляем знач-е
-            int number = x;  // и здесь присваиваем это знач-е переменной number
+            int secondNumber = x;  // и здесь присваиваем это знач-е переменной number
 
             do {
-                number = number / 10;
-                if(number < 100) {
+                secondNumber = secondNumber / 10;
+                if(secondNumber < 100) {
                     do {
-                        number = number - 10;
+                        secondNumber = secondNumber - 10;
                     }
-                    while(number > 10);
+                    while(secondNumber > 10);
                 }
             }
-            while(number > 100);
-            int n = int.Parse(x.ToString().Remove(1, 1) + number);  // перестановка 2-й цифры в конец числа; "Remove" - убир. эл-т в строке. 1я цифра - индекс, 2я - ск-ко эл-в убрать 
+            while(secondNumber > 100);
+            int n = int.Parse(x.ToString().Remove(1, 1) + secondNumber);  // перестановка 2-й цифры в конец числа; "Remove" - убир. эл-т в строке. 1я цифра - индекс, 2я - ск-ко эл-в убрать 
             Console.Write("результат: " + n);  // вывод результат
             Console.ReadKey();
         }
