20e89c2efa2817754fd7da172975fde8f2527ebc
a_лина <118667639+naflexsil@users.noreply.github.com>
2023-02-13 12:59:20 +0700

    Update Circus.cs

--- рмяяяяяяяяу/Circus.cs
+++ рмяяяяяяяяу/Circus.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace Circus {
+    class Circus {
+        static void Main(string[] args) {  // "string[] args" - классу можно передать массив со строками
+            Console.Write("введите a - ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("введите n - ");
+            int n = int.Parse(Console.ReadLine());
+
+            // результат возведения "a" в степень "n"
+            double result = 1;
+
+            // цикл для умножения result на "a" n-ое количество
+            for(int index = 0; index < n; ++index) {
+                result *= a;
+            }
+            Console.Write("результат = " + result);
+            Console.ReadKey();
+        }
+    }
+}
